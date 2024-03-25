54274f57ce9328180e2c9b487cbcb9a9c3770989
picklerick213 <164865394+picklerick213@users.noreply.github.com>
2024-03-26 02:40:38 +0800

    Delete src/app/api directory

--- src/app/api/upload/route.js
+++
@@ -1,8 +0,0 @@
-export async function POST(req) {
-    const data = await req.formData();
-    if (data.get('file')){
-        ///upload file 
-      const file = data.get('data');  
-    }
-    return Response.json(true);
-}
